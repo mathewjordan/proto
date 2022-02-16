ad13e08ce81740210dc9cfac7c14791769d5f61e
Mat Jordan <mat@northwestern.edu>
2022-02-16 15:24:54 -0500

    Add stitches and radix colors.

---
+++ stitches.config.js
@@ -0,0 +1,29 @@
+import { gray, blue, red, green } from "@radix-ui/colors";
+
+import { createStitches } from "@stitches/react";
+
+export const {
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  config,
+} = createStitches({
+  theme: {
+    colors: {
+      ...gray,
+      ...blue,
+      ...red,
+      ...green,
+    },
+  },
+  media: {
+    bp1: "(min-width: 480px)",
+  },
+  utils: {
+    marginX: (value) => ({ marginLeft: value, marginRight: value }),
+  },
+});
