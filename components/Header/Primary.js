4eab84e9b0155591271b762da7cf56b48c0eb5ab
Mat Jordan <mat@northwestern.edu>
2022-02-17 11:55:26 -0500

    Handle search value and focus state.

--- components/Header/Primary.js
+++ components/Header/Primary.js
@@ -1,19 +1,25 @@
+import { useState } from "react";
+import Sticky from "react-sticky-el";
+import { Primary } from "./Header.styled";
 import Nav from "../Nav/Nav";
 import Search from "../Search/Search";
-import { Primary } from "./Header.styled";
-import Sticky from "react-sticky-el";
 
 export default function HeaderPrimary() {
+  const [searchActive, setSearchActive] = useState(false);
+  const handleIsSearchActive = (status) => {
+    setSearchActive(status);
+  };
+
   return (
     <Sticky stickyClassName="sticky-primary">
-      <Primary>
+      <Primary data-search-active={searchActive}>
         <span>Northwestern</span>
         <div>
           <Nav>
             <a>Items</a>
             <a>Collections</a>
           </Nav>
-          <Search />
+          <Search isSearchActive={handleIsSearchActive} />
         </div>
       </Primary>
     </Sticky>
--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,15 +1,37 @@
+import { useEffect, useState } from "react";
 import { Button, Input, Wrapper } from "./Search.styled";
 
-export default function Search() {
+export default function Search({ isSearchActive }) {
+  const [searchValue, setSearchValue] = useState("");
+  const [searchFocus, setSearchFocus] = useState(false);
+
+  const handleSearchFocus = (e) => {
+    e.type === "focus" ? setSearchFocus(true) : setSearchFocus(false);
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  useEffect(() => {
+    !searchFocus && !searchValue ? isSearchActive(false) : isSearchActive(true);
+  }, [searchFocus, searchValue]);
+
   return (
     <Wrapper>
-      <Input placeholder="Search by keyword or phrase, ex: Berkeley Music Festival " />
+      <Input
+        placeholder="Search by keyword or phrase, ex: Berkeley Music Festival"
+        onChange={handleSearchChange}
+        onFocus={handleSearchFocus}
+        onBlur={handleSearchFocus}
+      />
       <Button>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <title>Search</title>
           <path d="M456.69 421.39L362.6 327.3a173.81 173.81 0 0034.84-104.58C397.44 126.38 319.06 48 222.72 48S48 126.38 48 222.72s78.38 174.72 174.72 174.72A173.81 173.81 0 00327.3 362.6l94.09 94.09a25 25 0 0035.3-35.3zM97.92 222.72a124.8 124.8 0 11124.8 124.8 124.95 124.95 0 01-124.8-124.8z" />
         </svg>
       </Button>
+      {/* <Button>clear</Button> */}
     </Wrapper>
   );
 }
